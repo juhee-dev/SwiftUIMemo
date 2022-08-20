3f427752eed1ba1f9130825271eff692278c13fd
juhee-dev <viimv2000@gmail.com>
2022-08-21 02:55:27 +0900

    Initial Commit

---
+++ Shared/SwiftUIMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIMemoApp.swift
+//  Shared
+//
+//  Created by juhee on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
