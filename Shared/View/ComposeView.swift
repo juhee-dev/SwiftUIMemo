f64c18a9058c816c70e00a466919b5a7e2c8f4fc
juhee-dev <viimv2000@gmail.com>
2022-08-24 00:28:59 +0900

    Step8 메모 편집

--- Shared/View/ComposeView.swift
+++ Shared/View/ComposeView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct ComposeView: View {
     // MemoStore 속성으로 메모 저장
     @EnvironmentObject var store: MemoStore
+    
+    // 여기에 memo가 전달되면 편집모드, 그렇지 않으면 쓰기모드
+    var memo: Memo? = nil
+    
     @Environment(\.dismiss) var dismiss
+    
     // context를 binding
     @State private var content: String = ""
 
@@ -20,8 +25,13 @@ struct ComposeView: View {
                 // 입력한 내용이 content에 자동 저장됨. 반대로 content에 값을 저장하면 TextEditor에서 보여짐
                 TextEditor(text: $content)
                     .padding()
+                    .onAppear {
+                        if let memo = memo {
+                            content = memo.content
+                        }
+                    }
             }
-            .navigationTitle("새 메모")
+            .navigationTitle(memo != nil ? "메모편집" : "새 메모")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 // navigationBarLeading: 왼쪽
@@ -36,8 +46,13 @@ struct ComposeView: View {
                 // navigationBarTrailing: 오른쪽
                 ToolbarItemGroup(placement: .navigationBarTrailing) {
                     Button {
-                        // 메모 저장소에 저장
-                        store.insert(memo: content)
+                        if let memo = memo {
+                            // 메모 편집
+                            store.update(memo: memo, content: content)
+                        } else {
+                            // 새 메모를 저장소에 저장
+                            store.insert(memo: content)
+                        }
                         
                         dismiss()
                     } label: {
--- Shared/View/DetailView.swift
+++ Shared/View/DetailView.swift
@@ -13,6 +13,8 @@ struct DetailView: View {
     
     @EnvironmentObject var store: MemoStore
     
+    @State private var showComposer = false
+    
     var body: some View {
         VStack {
             ScrollView {
@@ -34,12 +36,27 @@ struct DetailView: View {
         }
         .navigationTitle("메모 보기")
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "square.and.pencil")
+                }
+            }
+        }
+        .sheet(isPresented: $showComposer) {
+            // initialize로 메모 전달
+            ComposeView(memo: memo)
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(memo: Memo(content: "Hello"))
-            .environmentObject(MemoStore())
+        NavigationView {
+            DetailView(memo: Memo(content: "Hello"))
+                .environmentObject(MemoStore())
+        }
     }
 }
